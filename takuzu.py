6a5b54d118aaba842852f0fd8ffe044437595f0e
Martim Baltazar <martimnunesbaltazar@gmail.com>
2022-05-29 18:11:31 +0100

    commit result+goal_test

--- takuzu.py
+++ takuzu.py
@@ -8,6 +8,8 @@
 from sys import stdin
 
 import sys
+
+from numpy import append
 from search import (
     Problem,
     Node,
@@ -73,8 +75,74 @@ class Board:
         else: 
             column_left = None
         return (column_left,column_right)
-        # TODO
         pass
+
+    def possible_actions(self, row: int, col:int,number:int) -> [(int,int,int)]:
+        """Decide se uma ação é válida num determinado board"""
+        
+        # Verifica se horizontais sao iguais
+        if (self.adjacent_horizontal_numbers(row,col) == (number,number)):
+            return None
+
+        # Verifica se verticais sao iguais
+        if (self.adjacent_vertical_numbers(row,col) == (number,number)):
+            return None
+
+        n = len(self.board_lst)
+        var = 0
+        lim = n // 2
+
+        if (n % 2) != 0:
+            var = -1
+
+        # Verifica duas filas para cima
+        if (row > 2):
+            if (number == int(self.get_number(row-1,col)) 
+            and number == int(self.get_number(row-2,col))):
+                return None
+
+        # Verifica duas filas para baixo
+        if (row < n-2+var):
+            if (number == int(self.get_number(row+1,col)) 
+            and number == int(self.get_number(row+2,col))):
+                return None
+        
+        # Verifica duas colunas à esquerda
+        if(col > 2):
+            if (number == int(self.get_number(row,col-1)) 
+            and number == int(self.get_number(row,col-2))):
+                return None
+
+        # Verifica duas colunas à direita
+        if (col < n-2+var):
+            if (number == int(self.get_number(row,col+1)) 
+            and number == int(self.get_number(row,col+2))):
+                return None
+
+        # Verificar se pode "number" na linha
+        counter = 0
+        for a in range(0,n):
+            if board.board_lst[row][a] == str(number):
+                if (number != int(self.get_number(row,col))):
+                    counter += 1
+        if (counter >= lim):
+            print("C:",counter)
+            print("NONE: ")
+            return None
+
+        # Verificar se pode "number" na coluna
+        counter = 0
+        for a in range(0,n):
+            if board.board_lst[a][col] == str(number):
+                if (number != int(self.get_number(row,col))):
+                    counter += 1
+        if (counter >= lim):
+            print("C:",counter)
+            print("NONEcoluna: ")
+            return None
+
+        return (row,col,number)  
+    pass
 
     @staticmethod
     def parse_instance_from_stdin():
@@ -95,7 +163,6 @@ class Board:
         while (number1 != 0):
             line = stdin.readline()
             line_split = line.split("   ")
-            print(line_split)
             board_list.append(line_split)
             number1 -= 1
             
@@ -107,8 +174,6 @@ class Board:
                 else:
                     boardstate.board_str = boardstate.board_str + board_list[a][b]
         return boardstate
-        
-        # TODO
         pass
     def __str__ (self):
         return str(self.board_str)
@@ -119,13 +184,27 @@ class Board:
 class Takuzu(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
+        self.board = board
         # TODO
+
         pass
 
     def actions(self, state: TakuzuState):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
+        actions = []
+        size = len(state.board.board_lst)
+        for a in range(0,size):
+            for b in range(0,size):
+
+                if state.board.get_number(a,b) == "2" or state.board.board_lst[a][b] == "2\n":
+                    if (state.board.possible_actions(a,b,0)) != None:
+                        actions.append(state.board.possible_actions(a,b,0))
+                    if (state.board.possible_actions(a,b,1)) != None:
+                        actions.append(state.board.possible_actions(a,b,1))
         # TODO
+        print(actions)
+        return actions
         pass
 
     def result(self, state: TakuzuState, action):
@@ -133,14 +212,32 @@ class Takuzu(Problem):
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-        # TODO
+        state.board.board_lst[action[0]][action[1]] = action[2]
+        size = len(state.board.board_lst)
+
+        str_list = list(state.board.board_str)
+        str_list[action[0]*2*size+action[1]*2] = str(action[2])
+        new_board_str="".join(str_list)
+        state.board.board_str = new_board_str
+        
+        #print("\n")
+        #print(state.board.board_str,"\n")
+        return TakuzuState(board)
         pass
 
     def goal_test(self, state: TakuzuState):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas com uma sequência de números adjacentes."""
-        # TODO
+        size = len(state.board.board_lst)
+        for a in range(0,size):
+           for b in range(0,size):
+                #print(a,b)
+                if (state.board.possible_actions(a,b,int(state.board.get_number(a,b))) == None):
+                    return False
+        return True
+                
+                        
         pass
 
     def h(self, node: Node):
@@ -160,14 +257,22 @@ if __name__ == "__main__":
     
     board = Board.parse_instance_from_stdin() 
     print("Initial:\n", board, sep="")
-
     problem = Takuzu(board)
-    initial_state = TakuzuState(board)
-    # Mostrar valor na posição (2, 2):
-    print(initial_state.board.get_number(2, 2)) 
-    print(board.adjacent_vertical_numbers(3, 3))
-    print(board.adjacent_horizontal_numbers(3, 3))
-    print(board.adjacent_vertical_numbers(1, 1))
-    print(board.adjacent_horizontal_numbers(1, 1))
+    s0 = TakuzuState(board)
+    #problem.actions(initial_state)  
+    #print(initial_state.id)
+    
+    s1 = problem.result(s0, (0, 0, 0))
+    s2 = problem.result(s1, (0, 2, 1))
+    s3 = problem.result(s2, (1, 0, 1))
+    s4 = problem.result(s3, (1, 1, 0))
+    s5 = problem.result(s4, (1, 3, 1))
+    s6 = problem.result(s5, (2, 0, 0))
+    s7 = problem.result(s6, (2, 2, 1))
+    s8 = problem.result(s7, (2, 3, 1))
+    s9 = problem.result(s8, (3, 2, 0))
+    # Verificar se foi atingida a solução
+    print("Is goal?", problem.goal_test(s9))    
+    print("Solution:\n", s9.board, sep="")
 
     pass
